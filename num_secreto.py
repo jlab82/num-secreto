fe1f7aa96fa94bcbd5cd72964ec50f2b9f740483
jlab82 <joseluis@identifica-t.com>
2017-11-29 21:42:42 +0100

    no message

--- num_secreto.py
+++ num_secreto.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # mi número secreto es 9
 num_secreto = 9
-x = raw_input("Adivina el número secreto, tienes una oportunidad: ")
+x = raw_input("Adivina el número secreto del 1 al 20, tienes una oportunidad para ganar 300 maravedíes: ")
 x = int(x)
 print "Tu número seleccionado ha sido:"
 print x
